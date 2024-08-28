afcde9f8a0b5845d581d9aae40780fd091370a1c
Muhammad Numan <numanrasheed315@gmail.com>
2024-08-28 16:28:39 +0500

    Toggle button

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -1,13 +1,30 @@
 import React from 'react'
+import { useState } from "react";
+import styles from "./styles.module.css";
 
 function ColorScheme() {
+    const [isDark, setDark] = useState(false);
+
+
     return (
-        <div className="flex justify-center my-20 gap-3">
+        <div className="flex justify-center my-20 gap-3 items-center">
             Dark Mode Setting:
-            <label>
-                <input name="color-scheme" type="radio" value="light" checked /> Light</label>
-            <label>
-                <input name="color-scheme" type="radio" value="dark" /> Dark</label>
+            <label
+                className={styles.container}
+                title={isDark ? "Activate light mode" : "Activate dark mode"}
+                aria-label={isDark ? "Activate light mode" : "Activate dark mode"}
+            >
+                <input
+                    name="color-scheme"
+                    type="checkbox"
+                    value={isDark ? 'dark' : "light"}
+                    defaultChecked={isDark}
+                    // onClick={() => setDark(!isDark)}
+                    onChange={() => setDark(!isDark)}
+                />
+
+                <div />
+            </label>
         </div>
 
     )
